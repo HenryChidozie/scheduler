131d117b48883847bb2365874ae8ad0b44d265c1
HenryChidozie <chidoziechuks@hotmail.com>
2022-01-19 11:12:32 -0500

    Add DayList stories

--- stories/index.js
+++ stories/index.js
@@ -8,7 +8,10 @@ import "index.scss";
 
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
+import DayList from "components/DayList";
 
+
+//Button Stories
 
 storiesOf("Button", module)
   .addParameters({
@@ -43,3 +46,38 @@ storiesOf("DayListItem", module)
     // action() allows us to create a callback that appears in the actions panel when clicked
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> 
   ));
+
+
+  //DayList stories
+
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+  
+  storiesOf("DayList", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    })
+    .add("Monday", () => (
+      <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    ))
+    .add("Tuesday", () => (
+      <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    ))
+    .add("Wednesday", () => (
+        <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+    ));
