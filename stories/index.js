323cc74398cd341a5ccd2776d5c6b010cd25ff2c
HenryChidozie <chidoziechuks@hotmail.com>
2022-01-19 00:23:08 -0500

    Implement props in DayListItem component

--- stories/index.js
+++ stories/index.js
@@ -5,7 +5,10 @@ import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
+
 import Button from "components/Button";
+import DayListItem from "components/DayListItem";
+
 
 storiesOf("Button", module)
   .addParameters({
