d93a4995ad469284fa799481187d9f6132806403
HenryChidozie <chidoziechuks@hotmail.com>
2022-01-19 11:26:10 -0500

    Refactor DayList to dynamically create an array of DayListItem component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,31 +2,19 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 
-export default function DayList(props){
+export default function DayList(props) {
+
+  const dayListItems = (props.days || []).map((dayData, _index) => 
+    <DayListItem
+      key={dayData.id}
+      name={dayData.name}
+      spots={dayData.spots}
+      selected={props.selectedDay === dayData.id}
+      setDay={(_event) => props.setDay(dayData.id)}
+    />
+  );
 
   return(
-    <ul>
-      <DayListItem 
-        key={props.days[0].id}
-        name={props.days[0].name} 
-        spots={props.days[0].spots} 
-        selected={props.days[0].name === props.day}
-        setDay={props.setDay}  
-      />
-      <DayListItem
-        key={props.days[1].id} 
-        name={props.days[1].name} 
-        spots={props.days[1].spots} 
-        selected={props.days[1].name === props.day}
-        setDay={props.setDay}  
-      />
-      <DayListItem 
-        key={props.days[2].id}
-        name={props.days[2].name}
-        spots={props.days[2].spots} 
-        selected={props.days[2].name === props.day}
-        setDay={props.setDay}  
-      />      
-    </ul>
-  )
+    <ul>{dayListItems}</ul>
+  );
 }
