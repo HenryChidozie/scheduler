77e8dcc23e76a7f7cc367b2dbc216a95d5ad9a64
HenryChidozie <chidoziechuks@hotmail.com>
2022-01-19 14:01:04 -0500

    Refactor InterviewerListItem component to be dynamic

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -7,14 +7,27 @@ import "./InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
 
+  const interviewerListItemClass = classNames(
+    "interviewers__item", {
+    "interviewers__item--selected": props.selected
+    });
+
+  const interviewerListItemImageClass = classNames(
+    "interviewers__item-image", {
+    "interviewers__item--selected-image": props.selected
+    });
+
   return (
-    <li className="interviewers__item">
+    <li 
+      className={interviewerListItemClass}
+      onClick={props.setInterviewer}
+    >
       <img
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        className={interviewerListItemImageClass}
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
   );
 }
