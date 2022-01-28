9ba6836f25a47ed844d58df1b7a3c00983ad815e
HenryChidozie <chidoziechuks@hotmail.com>
2022-01-28 12:42:13 -0500

    Component updated

--- src/components/Appointment/Confirm.jsx
+++ src/components/Appointment/Confirm.jsx
@@ -1,17 +1,15 @@
 //component that confirms appointments
-import React from 'react';
-import Button from "../Button";
-
+import React from "react";
+import Button from "components/Button";
 
 export default function Confirm(props) {
-  const { message, onConfirm, onCancel } = props;
   return (
     <main className="appointment__card appointment__card--confirm">
-      <h1 className="text--semi-bold">{message}</h1>
+      <h1 className="text--semi-bold">Are you sure you would like to delete?</h1>
       <section className="appointment__actions">
-        <Button danger onClick={() => onCancel(false)}>Cancel</Button>
-        <Button danger onClick={() => onConfirm(true)}>Confirm</Button>
+        <Button danger onClick={props.onCancel}>Cancel</Button>
+        <Button danger onClick={props.onConfirm}>Confirm</Button>
       </section>
     </main>
   );
-};
+}
--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
+//import axios from "axios";
 
 import "index.scss";
 
 import Application from "components/Application";
 
-axios.defaults.baseURL = `${new URL (
-  process.env.REACT_APP_BASE_URL || window.location
-).origin}/api`;
+// axios.defaults.baseURL = `${new URL (
+//   process.env.REACT_APP_BASE_URL || window.location
+// ).origin}/api`;
 
 
 ReactDOM.render(<Application />, document.getElementById("root"));
