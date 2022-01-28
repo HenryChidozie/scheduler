e1c9d45dea55824ae6c17d84a236e0d9fc8fcb40
HenryChidozie <chidoziechuks@hotmail.com>
2022-01-28 11:47:11 -0500

    Refactor formatSpots function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,32 +1,29 @@
-//DayListItem components to display days in the sidebar
 import React from "react";
-import "./DayListItem.scss";
-const classNames = require("classnames");
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
-
-export default function DayListItem(props) {
-  const {name, spots, selected, setDay} = props;
-  const dayClass = classNames("day-list__item", {
-    "day-list__item--selected": selected,
-    "day-list__item--full": !spots
-  });
-
-  const formatSpots = function(spots) {
-    let plural = "";
-    if (spots > 1) {
-      plural += "s"
-      return `${spots} spot${plural} remaining`
-    } else if (spots === 1) {
-      return `${spots} spot${plural} remaining`
-    } else {
-      return "no spots remaining"
+const DayListItem = props => {
+  const { spots, selected, name, setDay } = props;
+  const formatSpots = spots => {
+    if (spots === 1) {
+      return `${spots} spot `;
     }
-  }
-  
+    if (spots > 1) {
+      return `${spots} spots `;
+    }
+    return `no spots `;
+  };
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0
+  });
   return (
-    <li className={dayClass} onClick={() => setDay(name)} selected={selected} data-testid="day" >
-      <h2 className="text--regular">{name}</h2> 
-      <h3 className="text--light">{formatSpots(spots)}</h3>
-    </li>
+    <>
+      <li className={dayClass} onClick={() => setDay(name)} selected={selected}>
+        <h2 className="text--regular">{name}</h2>
+        <h3 className="text--light">{formatSpots(spots)}remaining</h3>
+      </li>
+    </>
   );
 };
+export default DayListItem;
