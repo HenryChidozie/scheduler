dc95fda273680bbff553ffe3594a8e28d3211f85
HenryChidozie <chidoziechuks@hotmail.com>
2022-01-28 11:59:04 -0500

    Refactor getInterview function

--- src/helpers/selectors.jsx
+++ src/helpers/selectors.jsx
@@ -1,105 +1,30 @@
-// // Helper functions for managing appointments and interviews.
-
-// // getAppointmentsForDay returns an array of appointment objects for the given day.
-
-// export const getAppointmentsForDay = (state, dayId) => {
-//   try {
-//     return state.days
-//       .find((stateDay) => stateDay.id === dayId)
-//       .appointments.map((appointmentId) => state.appointments[appointmentId]);
-//   } catch (err) {
-//     return [];
-//   }
-// };
-
-// // getInterviewersForDay returns an array of interviewer objects for the given day.
-
-// export const getInterviewersForDay = (state, dayId) => {
-//   try {
-//     return state.days
-//       .find((stateDay) => stateDay.id === dayId)
-//       .interviewers.map((interviewerId) => state.interviewers[interviewerId]);
-//   } catch (err) {
-//     return [];
-//   }
-// };
-
-// // getInterview returns a full interview object for an interview
-// //    object coming from the appointments API.
-
-// export const getInterview = (state, interview) => {
-//   try {
-//     return {
-//       student:     interview.student,
-//       interviewer: { ...state.interviewers[interview.interviewer] }
-//     };
-//   } catch (err) {
-//     return null;
-//   }
-// };
-
-
-// ==========================================================
-
-// const getAppointmentsForDay = function (state, day) {
-//   const filteredDay = state.days.find(days => days.name === day);
-//   if (filteredDay[0].length !== 0) {
-//     return filteredDay[0]["appointments"].map(ele => state["appointments"][ele]);
-//   } else {
-//     return [];
-//   }
-// };
-
-function getAppointmentsForDay(state,day) {
-  const filteredDays = state.days.filter(days => days.name === day);
-  const dateFound = filteredDays[0];
-  if (!dateFound) {
-    return [];
-  }
-  const filteredAppointments = dateFound.appointments.map(perApt => {
-    return state.appointments[perApt];
-  })
-  return filteredAppointments;
-}
-
-// const getAppointmentsById = (appointments, interview, id) => {  //interview = {studentName, interview: {id, name}}
-//   const index = appointments.findIndex(ele => ele.id === id);
-//   appointments[index]["interview"] = interview;
-//   return appointments;
-// }
-
-const getInterview = function(state, interview) {
-  return !interview ? null : {student: interview.student, interviewer: state.interviewers[interview.interviewer]};
-};
-
-// const getInterviewersForDay  = function (state, day) {
-//   const filteredInterviewer = state.days.filter(days => days.name === day);
-//   if (filteredInterviewer.length !== 0) {
-//     return filteredInterviewer[0]["interviewers"].map(ele => state["interviewers"][ele]);
-//   } else {
-//     return [];
-//   }
-// };
-
-
-function getInterviewersForDay(state, day) {
-  const { days, interviewers } = state;
-  console.log('days', days);
-  console.log('interviewers', interviewers);
-
+// Helper functions for managing appointments and interviews.
+export function getAppointmentsForDay(state, day) {
+  const { days, appointments } = state;
   const filteredDay = days.find(item => day === item.name);
   if (days.length < 1 || filteredDay === undefined) {
     return [];
   }
-  const daysInterviewers = filteredDay.interviewers.map(
-    interview => interviewers[interview]
-  );
-  return daysInterviewers;
+  const daysAppointment = filteredDay.appointments.map(id => appointments[id]);
+  return daysAppointment;
 }
 
-export { 
-  getAppointmentsForDay, 
-  getInterview, 
-  getInterviewersForDay,
-  //getAppointmentsById
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  const interviewObject = {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  };
+  return interviewObject;
+}
+
+export function getInterviewersForDay(state, day) {
+  const { days, interviewers } = state;
+  const filteredDay = days.find(item => item.name === day);
+  if (days.length < 1 || filteredDay === undefined) {
+    return [];
+  }
+  return  filteredDay.interviewers.map( id => interviewers[id] );
 }
