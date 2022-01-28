1194028aef49951ebb68c06636bebcd38b874aca
HenryChidozie <chidoziechuks@hotmail.com>
2022-01-28 11:50:46 -0500

    Refactor component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,32 +1,28 @@
-//Component showing list of interviewers
 import React from "react";
-import PropTypes from 'prop-types';
-import "./InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+import PropTypes from 'prop-types';
 
-export default function InterviewerList(props) {
-
-  InterviewerList.propTypes = {
-    interviewers: PropTypes.array,
-    setInterviewer: PropTypes.func.isRequired
-  };
-
-  const interviewerChecker = props.interviewers.map((interviewer, _index) => {
+export default function InterviewerList(props){
+  const interviewers = props.interviewers.map((interviewer,index) => {
     return (
-      <InterviewerListItem 
-        key={interviewer.id} 
-        name={interviewer.name} 
-        avatar={interviewer.avatar} 
-        selected={interviewer.id === props.value} 
-        setInterviewer={(_event) => props.onChange(interviewer.id)} 
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
   });
-  return ( 
+  return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewers</h4>
-      <ul className="interviewers__list">{interviewerChecker}</ul>
-    </section>
-  );
-};
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section> 
+  )
+}
 
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
